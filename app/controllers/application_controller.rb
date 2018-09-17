ed84aff17e2352c1da78ab4e16a2fbb1a04365ae
Lucas Morais <lucas.axm@gmail.com>
2018-09-17 05:35:22 -0300

    Front-end done. Using cards instead of tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :disable_repositories_link
+  before_action :disable_owners_link
 
   private
 
@@ -7,4 +8,7 @@ class ApplicationController < ActionController::Base
     @repositories_is_empty = Repository.first.nil?
   end
 
+  def disable_owners_link
+    @owners_is_empty = Owner.first.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :repositories, only: %i[index show]
-  resources :owners, only: :show
+  resources :owners, only: %i[index show]
   root 'trending#index'
   get '/search', action: :search, controller: :trending
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
